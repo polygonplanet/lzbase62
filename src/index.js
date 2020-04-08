b327b622282dec5f3e91d3497fb170178daa04a1
polygonplanet <polygon.planet.aqua@gmail.com>
2020-04-09 00:40:10 +0900

    Fix doc comments

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ exports.version = require('../package.json').version;
 /**
  * Compress data to a base 62(0-9a-zA-Z) encoded string
  *
- * @param {string|Buffer} data Input data
- * @param {Object=} [options] Options
+ * @param {string} data Input data
+ * @param {object} [options] Options
  * @return {string} Compressed data
  */
 exports.compress = function(data, options) {
@@ -18,7 +18,7 @@ exports.compress = function(data, options) {
  * Decompress data from a base 62(0-9a-zA-Z) encoded string
  *
  * @param {string} data Input data
- * @param {Object=} [options] Options
+ * @param {object} [options] Options
  * @return {string} Decompressed data
  */
 exports.decompress = function(data, options) {
