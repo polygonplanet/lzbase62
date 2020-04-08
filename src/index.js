eafcb6e6ad2e1c8765b8f320b54f0ab43b4e4f8e
polygonplanet <polygon.planet.aqua@gmail.com>
2020-04-08 17:54:44 +0900

    Add `version` property

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 var Compressor = require('./compressor');
 var Decompressor = require('./decompressor');
 
-var lzbase62 = {
-  /**
-   * Compress data to a base 62(0-9a-zA-Z) encoded string
-   *
-   * @param {string|Buffer} data Input data
-   * @param {Object=} [options] Options
-   * @return {string} Compressed data
-   */
-  compress: function(data, options) {
-    return new Compressor(options).compress(data);
-  },
-  /**
-   * Decompress data from a base 62(0-9a-zA-Z) encoded string
-   *
-   * @param {string} data Input data
-   * @param {Object=} [options] Options
-   * @return {string} Decompressed data
-   */
-  decompress: function(data, options) {
-    return new Decompressor(options).decompress(data);
-  }
+exports.version = require('../package.json').version;
+
+/**
+ * Compress data to a base 62(0-9a-zA-Z) encoded string
+ *
+ * @param {string|Buffer} data Input data
+ * @param {Object=} [options] Options
+ * @return {string} Compressed data
+ */
+exports.compress = function(data, options) {
+  return new Compressor(options).compress(data);
 };
 
-module.exports = lzbase62;
+/**
+ * Decompress data from a base 62(0-9a-zA-Z) encoded string
+ *
+ * @param {string} data Input data
+ * @param {Object=} [options] Options
+ * @return {string} Decompressed data
+ */
+exports.decompress = function(data, options) {
+  return new Decompressor(options).decompress(data);
+};
