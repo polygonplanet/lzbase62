bb891b7c6997d62564315751f7ac93c76170bb30
polygonplanet <polygon.planet.aqua@gmail.com>
2020-04-08 15:56:18 +0900

    Update copyright year

--- src/banner.js
+++ src/banner.js
@@ -1,6 +1,6 @@
 /*!
  * <%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>
- * Copyright (c) 2014-2018 <%= pkg.author %>
+ * Copyright (c) 2014-2020 <%= pkg.author %>
  * <%= pkg.homepage %>
  * @license <%= pkg.license %>
  */
